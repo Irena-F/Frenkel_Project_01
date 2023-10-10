ff9dd2e1d4c959d33ddbe07ce67b0da68877c3b5
Irena-F <Ira.frenkel17@gmail.com>
2023-10-10 10:17:48 -0600

    HW Code

---
+++ Code/Project_01.R
@@ -0,0 +1,34 @@
+#read in data
+lead_dat <- read.csv("DataRaw/lead-iq-01.csv")
+#Create boxplot
+boxplot(IQ ~ Smelter, data = lead_dat,
+        xlab = "Location Status", ylab = "IQ Level",
+        main = "Box Plot of IQ Level by Location",
+        col = c("Purple", "Pink"), 
+        names = c("Far", "Near"), 
+        outline = TRUE,
+        boxwex = 0.4,  # Adjust box width
+        at = c(1, 2),   # Set positions for "Far" and "Near"
+        ylim = c(0, 160))
+library(knitr)
+?kable
+
+# Calculate statistics for the 'Far' and 'Near' smelter groups
+mean_far <- round(mean(lead_dat$IQ[lead_dat$Smelter == "Far"]),2)
+mean_near <- round(mean(lead_dat$IQ[lead_dat$Smelter == "Near"]),2)
+sd_far <- round(sd(lead_dat$IQ[lead_dat$Smelter == "Far"]),2)
+sd_near <- round(sd(lead_dat$IQ[lead_dat$Smelter == "Near"]),2)
+sample_size_far <- round(sum(lead_dat$Smelter == "Far"),2)
+sample_size_near <- round(sum(lead_dat$Smelter == "Near"),2)
+
+# Create a data frame for the statistics
+tab <- data.frame(
+  Smelter = c("Far", "Near"),
+  SampleSize = c(sample_size_far, sample_size_near),
+  Mean = c(mean_far, mean_near),
+  SD = c(sd_far, sd_near)
+)
+
+#Create table
+
+kable(tab, align ='lcrr', format = 'simple', col.names = c("Smelter","Sample Size", "Mean IQ", "Standard Deviation"),caption ="Summary Statistics by Smelter Location Status")
